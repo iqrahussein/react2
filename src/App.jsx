f035374bfb9371b82993363329f088eabc2e3ccf
iqrahussein <iqramohamed600@gmail.com>
2026-06-26 20:22:53 +0300

    react component exercise

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Usercard from './Usercard'
+function App(){
+  return(
+    <>
+      <Usercard/>
+      <h1>hello world</h1> 
+    </>
+   
+  )
+  
+  
+  
+}
+
+export default App;
---
+++ src/Usercard.jsx
@@ -0,0 +1,10 @@
+const UserCard =()=>{
+  return(
+    <>
+    <h2>name:Iqra Hussein</h2>
+    <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed recusandae eligendi illum, ipsa minima nemo perspiciatis facere porro ab rem tempora. Ex pariatur minus magnam ipsum vero, deleniti vel ut?</p>
+    </>
+  )
+}
+
+export default UserCard;
